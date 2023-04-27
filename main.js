74237714d7ef535b84fe8840c6321bd438888a84
Tamo25 <gogatishvilit25@gmail.com>
2023-04-27 09:00:11 +0400

    changes on page

---
+++ main.js
@@ -0,0 +1,23 @@
+const isMobile = window.matchMedia("(max-width: 767px)");
+
+const menuBtn = document.querySelector(".menu");
+  const closeBtn = document.querySelector(".close");
+  const menuBox = document.querySelector(".menu-box");
+  const btn = document.getElementById("btn");
+  const btn2 = document.querySelector(".btn");
+
+ menuBtn.addEventListener("click", () => {
+   menuBtn.style.display = "none";
+   closeBtn.style.display = "block";
+    menuBox.style.display = "flex";
+    btn.style.display = "flex";
+    btn2.style.display = "flex";
+  });
+
+  closeBtn.addEventListener("click", () => {
+   closeBtn.style.display = "none";
+    menuBtn.style.display = "block";
+    menuBox.style.display = "none";
+   btn.style.display = "none";
+   btn2.style.display = "none";
+  });
